32468dcd232b8d5dfc20aece10553bb5f2a1ef85
Matthew Bramhall <mrbramhall@gmail.com>
2014-10-09 18:25:30 -0500

    Commit first ~10 lessons.

---
+++ Makefile
@@ -0,0 +1,6 @@
+CFLAGS=-Wall -g -ggdb
+
+clean:
+	rm -f ex1.exe
+	rm -f ex3.exe
+	rm -f ex4.exe
---
+++ ex1.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // necessary to use puts properly
+
+int main(int argc, char *argv[])
+{
+    puts("Hello world.");
+
+    return 0;
+}
